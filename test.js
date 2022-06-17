4dc2baa5cd577598ca3c13ac2c99ed84b041cfb6
bbbb <bgh9651@gmail.com>
2022-06-17 11:08:53 +0900

    test

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const evenNumber = () => {
-  let arr = [];
+  let arr1 = [];
 
   for (let i = 0; i < 51; i++) {
     if (i % 2 === 0) {
       arr.push(i);
     }
-    return arr;
+    return arr1;
   }
 };
